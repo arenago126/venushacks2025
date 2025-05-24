a655baaa14e0e22b9b2b5725fd4630a2b51c056e
arenago126 <arenagaleanaoregonn@gmail.com>
2025-05-24 16:29:44 -0700

    changed index

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,9 +5,9 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 export default function TabLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Tabs screenOptions={{ headerShown: false }} initialRouteName="dashboard">
+      <Tabs screenOptions={{ headerShown: false }} initialRouteName="index">
         <Tabs.Screen
-          name="dashboard"
+          name="index"
           options={{
             title: 'Dashboard',
             tabBarIcon: ({ color }) => <MaterialIcons name="home" size={24} color={color} />,
@@ -21,17 +21,17 @@ export default function TabLayout() {
           }}
         />
         <Tabs.Screen
-          name="my finances"
+          name="myfinances"
           options={{
-            title: 'my finances',
-            tabBarIcon: ({ color }) => <MaterialIcons name="settings" size={24} color={color} />,
+            title: 'Finances',
+            tabBarIcon: ({ color }) => <MaterialIcons name="account-balance" size={24} color={color} />,
           }}
         />
         <Tabs.Screen
           name="chatbot"
           options={{
             title: 'Chatbot',
-            tabBarIcon: ({ color }) => <MaterialIcons name="chat" size={24} color={color} />,
+            tabBarIcon: ({ color }) => <MaterialIcons name="comment-bank" size={24} color={color} />,
           }}
         />
       </Tabs>
